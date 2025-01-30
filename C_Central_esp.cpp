962f70b73040deaca78b7f1219ea75cc15869875
Hugo <151167535+Arvid459@users.noreply.github.com>
2025-01-30 12:29:58 +0100

    new working kode

--- C_Central_esp.cpp
+++ C_Central_esp.cpp
@@ -1,49 +1,85 @@
-#include <NTPClient.h>    //TID
+#include <NTPClient.h>     // TID
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
 #include <FirebaseESP8266.h>
 
+
 // WiFi-konfiguration
 #define WIFI_SSID "David - 15 PRO"
 #define WIFI_PASSWORD "Hej pa dig!!"
+
 
 // Firebase-konfiguration
 FirebaseData firebaseData;
 FirebaseConfig firebaseConfig;
 FirebaseAuth firebaseAuth;
 
+
+// Tid och UDP
 const long utcOffsetInSeconds = 3600;
-
-WiFiUDP udp;
-
-WiFiUDP ntpUDP; //TID
-NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds); //TID
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP, "pool.ntp.org", utcOffsetInSeconds); // TID
 
 
-
-int ESP_ID = -1; // Declare ESP_ID globally
-const int localPort = 12345; // Port för att lyssna på inkommande data
+// Global variabler
 const int esps = 1;
 const int numSensors = 4; // Antal sensorer
 const int maxMessages = 2; // Antal meddelanden att lagra för medelvärdesberäkning
 int sensorData[numSensors][maxMessages] = {0}; // Buffert för sensorvärden
 int messageCount = 0;
-int sensorDataDay[7][39]; //Day and in which time interval
+int sensorDataDay[7][39]; // Dag och tidsintervall
+
 
 unsigned long lastFirebaseUpdate = 0;  // För att hålla koll på tiden för Firebase-uppdatering
+WiFiUDP udp;
+char packetBuffer[255]; // Buffert för att läsa UDP-paket
 
-void extractData(String data){
 
-  // Extrahera ESP_ID (första siffran i den mottagna datasträngen)
-  int ESP_ID = data.charAt(0) - '0'; // Första siffran är ESP_ID
+void setup() {
+ Serial.begin(115200);
 
-  // Kontrollera att ESP_ID är giltigt (t.ex. inom ett förväntat intervall)
-  if (ESP_ID < 0 || ESP_ID >= esps) {
-    Serial.println("Ogiltigt ESP_ID: " + String(ESP_ID));
-  }
-  else{
-       // Lagra sensorvärden i bufferten för motsvarande ESP_ID
-        for (int j = 1; j < data.length(); j++){ //!OBS! DATA ÄR STRING, SE "DATA"
+
+ // Anslut till Wi-Fi
+ Serial.print("Connecting to Wi-Fi");
+ WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+ while (WiFi.status() != WL_CONNECTED) {
+   Serial.print(".");
+   delay(500);
+ }
+ Serial.println("\nWiFi connected");
+ Serial.print("Receiver IP: ");
+ Serial.println(WiFi.localIP());
+
+
+ // Konfigurera Firebase
+ firebaseConfig.host = "ultraljudsprojekt-default-rtdb.europe-west1.firebasedatabase.app";
+ firebaseConfig.signer.tokens.legacy_token = "lAbQ4CiEUevvpthB0hr0mEC346UJ7x4KuDTe4hyi";
+ Firebase.begin(&firebaseConfig, &firebaseAuth);
+
+
+ // Starta UDP
+ udp.begin(12345);
+ Serial.println("UDP mottagning startad");
+
+
+ // Starta NTP klienten
+ timeClient.begin();
+}
+
+void extractData(String data) {
+ // Extrahera ESP_ID från första siffran i datasträngen
+ int ESP_ID = data.charAt(0) - '0'; // Första siffran är ESP_ID
+
+
+ if (ESP_ID < 0 || ESP_ID > esps) {
+   Serial.println("Ogiltigt ESP_ID: " + String(ESP_ID));
+   return;  // Felaktigt ESP_ID, avbryt
+ }
+
+
+
+
+for (int j = 1; j < data.length(); j++){ //!OBS! DATA ÄR STRING, SE "DATA"
 
          int countZeros = 0;
          int countOnes = 0;
@@ -73,140 +109,90 @@ void extractData(String data){
         
           String strData = String(data); // Convert integer to string
           sensorData[ESP_ID][messageCount % maxMessages] = strData.substring(1).toInt(); // Remove the first character (digit)
-        }
+        
 
-        if (messageCount % maxMessages == 0){
-          updateDataDay();
-        }
-
-          messageCount++;
-
-      Serial.println("Data från ESP " + String(ESP_ID) + " lagrad.");
-
-      }
-
-void updateDataDay(){
-    int timestamp = static_cast<int>((timeClient.getHours() - 11) * 60 + timeClient.getMinutes() / 4) % 39;
-    int value = 0;
-
-  // Bygg upp en sträng med alla sensorvärden från båda ESP-enheterna
-  for (int esp = 0; esp < (esps); esp++) {
-    for (int messageNr = 0; messageNr < maxMessages; messageNr++) {
-          value += sensorData[esp][messageNr];
-        }
-  }
-
-    value = floor(value / (esps * maxMessages) * 10);
-    sensorDataDay[timeClient.getDay()][timestamp] = value;
-    updateFirebase(timestamp);
+    messageCount++;
+    Serial.println("Data från ESP " + String(ESP_ID) + " lagrad." + sensorData[ESP_ID][(messageCount -1) % maxMessages]);
 }
 
-void updateFirebase(int timestamp2) {
-  String firebaseDataString = "";
- // int timestamp = floor((timeClient.getHours() - 11) * 60 + timeClient.getMinutes() / 4); //!ÄNDRA 5MIN TILL ANPASSAT
-
-  if (timeClient.getHours()>14){                                                                                              //!!ÄNDRA 14! DETTA VÄRDE STÅR FÖR ATT NÄR KLOCKAN ÄR EFTER 14 SKA DEN PUSHA UPP DAGSPROGNOSEN!!
-  // Bygg upp en sträng med alla sensorvärden från båda ESP-enheterna
-  firebaseDataString += timeClient.getDay();
-
-    for (int cordinate = 0; cordinate < 39; cordinate++) {
-    firebaseDataString += sensorDataDay[timeClient.getDay()][cordinate];
-    }
-  
-  }
-  else{
-    if (timestamp2 >= 0 && timestamp2 <=39){
-    firebaseDataString = sensorDataDay[timeClient.getDay()][timestamp2];    //MAY NEED TO FIX 
-    }
-    else{
-    Serial.println("Error with upploading");
-    }
-  }
-
-  // Skicka data till Firebase
-  if (Firebase.pushString(firebaseData, "/espData", firebaseDataString)) {
-    Serial.println("Data skickad till Firebase: " + firebaseDataString);
-  } else {
-    Serial.println("Firebase error: " + firebaseData.errorReason());
-  }
-}
+void updateDataDay() {
+ int timestamp = (((timeClient.getHours() - 8) * 60 + timeClient.getMinutes()) * 3) % 39; // Beräkna tidsstämpel
+ int value = 0;
 
 
-void setup() {
- Serial.begin(115200);
-
- // Anslut till WiFi
- WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
- Serial.print("Connecting to Wi-Fi");
- while (WiFi.status() != WL_CONNECTED) {
-   Serial.print(".");
-   delay(500);
+ // Beräkna medelvärde för varje sensor och tidsintervall
+ for (int esp = 0; esp < esps; esp++) {
+   for (int messageNr = 0; messageNr < maxMessages; messageNr++) {
+     value += sensorData[esp][messageNr];
+   }
  }
- Serial.println("\nWiFi connected");
 
- // Skriv ut IP-adressen
- Serial.print("Receiver IP: ");
- Serial.println(WiFi.localIP());
 
- // Konfigurera Firebase
- firebaseConfig.host = "ultraljudsprojekt-default-rtdb.europe-west1.firebasedatabase.app";
- firebaseConfig.signer.tokens.legacy_token = "lAbQ4CiEUevvpthB0hr0mEC346UJ7x4KuDTe4hyi";
+ value = floor(value / (esps * maxMessages) * 10); // Normalisera värdet
+ sensorDataDay[timeClient.getDay()][timestamp] = value;
 
- Firebase.begin(&firebaseConfig, &firebaseAuth);
 
- // Starta UDP
- udp.begin(localPort);
- Serial.println("UDP mottagning startad");
+ // Uppdatera Firebase med det nya data
+ //updateFirebase(timestamp);                                                     //Kanske behöver vara med
+}
+
+
+void updateFirebase(int timestamp) {
+ String firebaseDataString = "";
+  // Kontrollera om det är efter 14:00, skicka hela dagsprognosen
+ if (timeClient.getHours() > 14) {
+   firebaseDataString += String(timeClient.getDay());
+   for (int i = 0; i < 39; i++) {
+     firebaseDataString += String(sensorDataDay[timeClient.getDay()][i]);
+   }
+ } else {
+   // Skicka uppdaterad data för specifikt tidsintervall
+   if (timestamp >= 0 && timestamp < 39) {
+     firebaseDataString = String(sensorDataDay[timeClient.getDay()][timestamp]);
+   } else {
+     Serial.println("Fel vid uppladdning av data");
+     return; // Om timestamp är utanför intervallet, avbryt
+   }
+ }
+
+
+ // Skicka data till Firebase
+ if (Firebase.pushString(firebaseData, "/espData", firebaseDataString)) {
+   Serial.println("Data skickad till Firebase: " + firebaseDataString);
+ } else {
+   Serial.println("Firebase error: " + firebaseData.errorReason());
+ }
 }
 
 void loop() {
+ // Hämta NTP-tid
+ timeClient.update();
 
-// Datan som tas emot är ex "10100" Första siffran är nummret på ESP ( ESP_ID) 
-// Femsiffriga numret är "data"
 
- char packetBuffer[255];
+ // Hantera UDP-data
  int packetSize = udp.parsePacket();
  if (packetSize) {
-   int len = udp.read(packetBuffer, 255);
+   int len = udp.read(packetBuffer, sizeof(packetBuffer) - 1);
    if (len > 0) {
-     packetBuffer[len] = 0;
+     packetBuffer[len] = 0;  // Lägg till null-terminering
    }
+
+
    String data = String(packetBuffer);
    Serial.println("Data mottagen: " + data);
-  
-  extractData(data);
+   extractData(data);
 
-   // Uppdatera Firebase var tredje sekund
- //  if (millis() - lastFirebaseUpdate >= 8000) { 
-  //    lastFirebaseUpdate = millis();
 
-  //    updateFirebase();
-/*
-     String averageData = String(ESP_ID); // Starta med ESP_ID (den första siffran)
-
-     // Beräkna medelvärde för varje sensor och lägg till i den femsiffriga koden
-     for (int i = 0; i < numSensors / 2; i++) {   //ÄNDRA OM INTE 8 SENSORER
-       int sum = 0;
-       for (int j = 0; j < maxMessages; j++) {
-          if (sensorData[j][i] === 2){
-
-          }
-          else{
-            sum += sensorData[j][i];
-          }
-       }
-       int average = sum / maxMessages; // Beräkna medelvärde
-       
-       averageData += String(average); // Lägg till medelvärde för den aktuella sensorn
-     }
-
-     // Skicka den femsiffriga koden till Firebase
-     if (Firebase.pushString(firebaseData, "/esp", averageData)) {
-       Serial.println("Data skickad till Firebase: " + averageData);
-     } else {
-       Serial.println("Firebase error: " + firebaseData.errorReason());
-     }
-     */
+   // Uppdatera dagdata var 20:e sekund
+   if (timeClient.getSeconds() % 20 == 0) {
+     updateDataDay();
    }
  }
-//}
+
+
+ // Uppdatera Firebase var tredje sekund
+ if (millis() - lastFirebaseUpdate >= 8000) {
+   lastFirebaseUpdate = millis();
+   updateFirebase(timeClient.getDay()); // Skicka uppdaterad dagdata till Firebase              /*FIX!!!!!!!!!!!*/
+ }
+}
